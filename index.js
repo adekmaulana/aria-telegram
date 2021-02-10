061c5b76127b75d602ad74c094e761a64e4c8981
Adek Maulana <adek@techdro.id>
2021-02-11 00:47:43 +0700

    bot: accept bot username eg: /start@usernamebot
    also following changes from ping, and 'use strict' on aria
    
    Change-Id: I049249836667facba07d7d9a4a7ab939dcf71065

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 'use strict'
 
 const { Telegraf } = require('telegraf')
+const Aria2 = require('aria2')
 const commands = require('./core/commands')
 // Init aria2c daemon
 require('./core/aria').init()
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
+const aria2 = new Aria2()
+const username = process.env.BOT_USERNAME
 
 
-bot.hears(/^\/([a-zA-Z]+)/i, (ctx) => {
-  let command = ctx.match[1]
-  if (command in commands) {
-    commands[command].execute(ctx, bot.telegram)
+bot.context = {
+  aria: aria2
+}
+
+const pattern = /^\/([a-zA-Z]+)(@.*bot)?(?: |$)(.*)/i
+bot.hears(pattern, (ctx) => {
+  if (ctx.match[2] !== username || !ctx.match[2]) {
+    let command = ctx.match[1]
+    if (command in commands) {
+      commands[command].execute(ctx)
+    }
   }
 })
 
